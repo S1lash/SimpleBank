27e088898efda66b54f943791cbd6da8c8a40a0f
Ilya Kuzmichev <mg20118@gmail.com>
2018-06-20 21:03:05 +0300

    init commit

---
+++ src/main/java/ru/kuzmichev/SimpleBank/SimpleBankApplication.java
@@ -0,0 +1,12 @@
+package ru.kuzmichev.SimpleBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankApplication.class, args);
+	}
+}
