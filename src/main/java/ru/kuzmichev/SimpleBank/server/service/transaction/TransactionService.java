9cfce4a5f17e191565cecafb0845a091c325cab2
Ilya Kuzmichev <mg20118@gmail.com>
2018-06-22 00:45:56 +0300

    implemented Transaction-service level

--- src/main/java/ru/kuzmichev/SimpleBank/server/service/transaction/TransactionService.java
+++ src/main/java/ru/kuzmichev/SimpleBank/server/service/transaction/TransactionService.java
@@ -1,4 +1,26 @@
 package ru.kuzmichev.SimpleBank.server.service.transaction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import ru.kuzmichev.SimpleBank.server.service.transaction.repository.TransactionRepository;
+
+import static ru.kuzmichev.SimpleBank.server.util.Converters.convert;
+
+@Service
 public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Transactional(propagation = Propagation.MANDATORY)
+    public void saveAccount(Transaction transaction) {
+        Assert.notNull(transaction, "transaction is null");
+        Assert.notNull(transaction.getDebitAccount(), "transaction.debitAccount is null");
+        Assert.notNull(transaction.getCreditAccount(), "transaction.creditAccount is null");
+
+        transactionRepository.save(convert(transaction));
+    }
 }
