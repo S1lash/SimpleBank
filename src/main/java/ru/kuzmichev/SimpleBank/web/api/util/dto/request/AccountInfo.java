22019d0a227f8cee420d002672baa8d472e2a7fa
Ilya Kuzmichev <mg20118@gmail.com>
2018-06-25 10:24:49 +0300

    fixed

--- src/main/java/ru/kuzmichev/SimpleBank/web/api/util/dto/request/AccountInfo.java
+++ src/main/java/ru/kuzmichev/SimpleBank/web/api/util/dto/request/AccountInfo.java
@@ -1,9 +1,11 @@
 package ru.kuzmichev.SimpleBank.web.api.util.dto.request;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.jetbrains.annotations.Nullable;
 
 @Data
+@Accessors(chain = true)
 public class AccountInfo {
     @Nullable
     private Long clientId;
